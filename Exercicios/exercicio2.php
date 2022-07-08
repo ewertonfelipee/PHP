4f3d33455e56c159512c3425b5d265fd7ae7f3e2
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-07-07 21:06:18 -0300

    Folder creation

---
+++ Exercicios/exercicio2.php
@@ -0,0 +1,13 @@
+<?php
+
+    //  nome
+    $name = "Ewerton <br>";
+    //  idade
+    $role = "student <br>";
+    //  profissao
+    $age = 24;
+
+    echo $name;
+    echo $role;
+    echo $age;
+?>
