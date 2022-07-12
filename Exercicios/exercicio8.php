c99442fd1af5b2ddd6322af2fa3ea48b997f7be3
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-07-12 17:07:51 -0300

    Exercicios 8 and 9

---
+++ Exercicios/exercicio8.php
@@ -0,0 +1,12 @@
+<?php
+//  criar 3 variaveis de tipos diferentes
+    $var1 = 4;
+    $var2 = 2.5;
+    $var3 = "string";
+
+    echo $var1;
+    echo "<br>";
+    echo $var2;
+    echo "<br>";
+    echo $var3;
+    echo "<br>";
