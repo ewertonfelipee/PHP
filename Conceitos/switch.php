a1e7a2dcb312967ecf6c49b56f0c98719362c307
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-07-17 17:07:52 -0300

    Switch concepts

---
+++ Conceitos/switch.php
@@ -0,0 +1,20 @@
+<?php
+
+  $favcolor = "blue";
+
+  switch($favcolor){
+    case "red":
+      echo "Your fav color is red!";
+      break;
+
+    case "blue":
+      echo "Your fav color is blue!";
+      break;
+
+    case "black":
+      echo "Your fav color is black!";
+      break;
+
+    default:
+      echo "your fav color don't be here";
+  }
