adf461f4b67f8114fdf5674d9f12aee977b70417
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-08-15 14:23:00 -0300

    Comparacao de uma string com um inteiro
    Essa comparacao mostra que se for feita uma comparacao do tipo Loose, a string vai ser igual a um inteiro podendo gerar erros perigosos para o sistema

---
+++ Conceitos/stringWithIntComparison.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+    $key = "sdfdfsdf";
+    $chave = 0;
+    
+    if($key === $chave){
+        echo "Entrou no if";
+    }
+    else{
+        echo "nao entrou no if";
+    }
